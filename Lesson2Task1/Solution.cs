4f8259701e42a047e2140f81f59473dc144211b4
DMITRIJ MAMAJEV <mokelok@live.ru>
2017-04-05 23:55:45 +0300

    Lesson 1 created - BinaryGap. Lesson 2 OddOccurrencesInArray created with solution. Lesson 2 CyclicRotation created, but solution need to be rechecked.

---
+++ Lesson2Task1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2Task1
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            //Solution is based on bitwise XOR
+            //http://www.ardendertat.com/2011/12/13/programming-interview-questions-22-find-odd-occurring-element/
+            //http://www.geeksforgeeks.org/find-the-two-numbers-with-odd-occurences-in-an-unsorted-array/
+
+            var result = 0;
+            for (int i = 0; i < A.Length; i++) 
+            {
+                result ^= A[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Lesson2Task1Tests/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lesson2Task1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2Task1.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void solutionTest()
+        {
+            var A = new[] {9, 3, 9, 3, 7, 5, 5, 9, 9};
+            Solution sol = new Solution();
+            var test = sol.solution(A);
+
+            Assert.AreEqual(7, test);
+        }
+    }
+}
